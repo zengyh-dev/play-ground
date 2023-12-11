f866deede039754790d9778f5f8f78bfab919145
Leo <2493765289@qq.com>
2023-12-11 23:42:17 +0800

    feat: pointedLight

--- packages/graphics/src/main.tsx
+++ packages/graphics/src/main.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import Zfighting from "./webgl/6_Light/33_LightedTranslatedRotatedCube";
+import Zfighting from "./webgl/6_Light/34_PointLightedCube";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<Zfighting />);
